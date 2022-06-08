1bc5f3b5484ef76301a04d0fb324418f54e33a1d
davibordon <davi.silva@sptech.school>
2022-06-07 23:35:47 -0300

    Projeto Finalizado

--- Site Institucional/Projeto-Individual-EBC_v2/site/src/database/config.js
+++ Site Institucional/Projeto-Individual-EBC_v2/site/src/database/config.js
@@ -5,8 +5,8 @@ var sql = require('mssql');
 var mySqlConfig = {
     host: "localhost",
     database: "EBC",
-    user: "aluno",
-    password: "sptech",
+    user: "root",
+    password: "123456",
 };
 
 function executar(instrucao) {
